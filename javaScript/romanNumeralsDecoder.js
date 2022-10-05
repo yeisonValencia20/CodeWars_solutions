f65d8fabd439bc481e7ef59b796a1fb78e087608
YEISON VALENCIA GOMEZ <yeison.vg@outlook.com>
2022-10-04 21:29:35 -0500

    roman numerals decoder

---
+++ javaScript/romanNumeralsDecoder.js
@@ -0,0 +1,95 @@
+// Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+// Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+// Example:
+
+// solution('XXI'); // should return 21
+// Help:
+
+// Symbol    Value
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1,000
+
+// MI CODIGO
+function solution (roman) {
+    // complete the solution by transforming the 
+    // string roman numeral into an integer
+    let romanNumbers = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    let previousNumber;
+    let sum = 0;
+    for(let i=0; i<roman.length; i++){
+        if(previousNumber < romanNumbers[roman[i]]){
+            sum -= previousNumber*2;
+        }
+        sum += romanNumbers[roman[i]];
+        previousNumber = romanNumbers[roman[i]];
+    }
+    return sum
+}
+
+// CODIGO CON MEJORES PRACTICAS
+function solution(roman)
+{
+  var data = {M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1};
+  var numbers = roman.split('');
+  var sum = 0, i;
+
+  for(i = 0; i < numbers.length; i++)
+  {
+    if(data[numbers[i]] < data[numbers[i+1]])
+    {
+      sum += data[numbers[i+1]] - data[numbers[i]];
+      i++;
+    }
+    else
+    {
+      sum += data[numbers[i]];
+    }
+  }
+  
+  return sum;
+}
+
+function solution(roman){
+    var conversion = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
+    
+    return roman.match(/CM|CD|XC|XL|IX|IV|\w/g).reduce((accum, roman) => accum + conversion[roman], 0);
+ }
+
+ function solution(roman) {
+    var value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+    return roman.split('').map( d => value[d] ).reduce( (s,v,i,o) => s + ( (o[i+1] > v) ? -v : v ), 0 );
+}
+
+const numeralMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  
+  function solution(roman){
+    const arrNums = roman.split('').map((el) => numeralMap[el]);
+    
+    return arrNums.reduce((sum, cur, i, a) => {
+      if (cur < a[i+1]) return sum - cur;
+      return sum + cur;
+    }, 0)
+  }
