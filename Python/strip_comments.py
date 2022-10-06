d5cbe88c5b807a2ac30a940caf31dc7f6ce483fd
YEISON VALENCIA GOMEZ <yeison.vg@outlook.com>
2022-10-05 19:35:25 -0500

    strip comments

--- Python/strip_comments.py
+++ Python/strip_comments.py
@@ -16,16 +16,40 @@
 
 # result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
 # # result should == "apples, pears\ngrapes\nbananas"
+# MI CODIGO
 import re
 import codecs
 def strip_comments(strng, markers):
     txt = repr(strng)[1:-1] #convert string to raw string, for escape special characters
     txt = re.sub('\\t','',txt)
     for i in markers:
-        regex = '\s*\\' + i + '(\s*[#\',^.\-!@?]*[a-z]*[#\',^.\-!@?]*){1,}' 
+        regex = '\s*\\' + i + '(\s*[#\',^.\-!@?=]*[a-z]*[#\',^.\-!@?=]*){1,}' 
         txt = re.sub(regex, '', txt)
     
     txt = codecs.decode(txt, 'unicode_escape') #convert to normal string
     return txt
 
 strip_comments('\t! @ !\nstrawberries ! avocados ! = ^\noranges oranges', ["'", '=', '@', '.', '^', '#', '?', '-', '!'])
+
+
+#CODIGO CON MEJORES PRACTICAS
+def solution(string,markers):
+    parts = string.split('\n')
+    for s in markers:
+        parts = [v.split(s)[0].rstrip() for v in parts]
+    return '\n'.join(parts)
+
+def strip_line(line, markers):
+    for m in markers:
+        if m in line:
+            line = line[:line.index(m)]
+    return line.rstrip()
+def solution(string,markers):
+    stripped = [strip_line(l, markers) for l in string.splitlines()]
+    return '\n'.join(stripped)
+
+def strip_comments(strng, markers):
+    for num, marker in enumerate(markers):
+        strng = '\n'.join([i.split(marker)[0].rstrip() for i in strng.split('\n')])
+
+    return strng
