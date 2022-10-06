b88c151eb9dd101757ac4371fa6452d14ca303cf
YEISON VALENCIA GOMEZ <yeison.vg@outlook.com>
2022-10-06 17:50:16 -0500

    picks peaks

---
+++ Python/pick_peaks.py
@@ -0,0 +1,77 @@
+# In this kata, you will write a function that returns the positions and the values of the "peaks" (or local maxima) of a numeric array.
+
+# For example, the array arr = [0, 1, 2, 5, 1, 0] has a peak at position 3 with a value of 5 (since arr[3] equals 5).
+
+# The output will be returned as an object with two properties: pos and peaks. Both of these properties should be arrays. If there is no peak in the given array,
+#  then the output should be {pos: [], peaks: []}.
+
+# Example: pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]) should return {pos: [3, 7], peaks: [6, 3]} (or equivalent in other languages)
+
+# All input arrays will be valid integer arrays (although it could still be empty), so you won't need to validate the input.
+
+# The first and last elements of the array will not be considered as peaks (in the context of a mathematical function, we don't know what is after and before and therefore,
+#  we don't know if it is a peak or not).
+
+# Also, beware of plateaus !!! [1, 2, 2, 2, 1] has a peak while [1, 2, 2, 2, 3] and [1, 2, 2, 2, 2] do not. In case of a plateau-peak, please only return the position and value of
+#  the beginning of the plateau.
+#  For example: pickPeaks([1, 2, 2, 2, 1]) returns {pos: [1], peaks: [2]} (or equivalent in other languages)
+
+# MI CODIGO
+def pick_peaks(arr):
+    picks_object = { "pos": [], "peaks":[] }
+    is_plateous = False
+    for i in range(len(arr))[1:-1]:
+        if(not arr[i] > arr[i-1] and not is_plateous):
+            continue
+
+        if(arr[i] > arr[i+1]):
+            if(is_plateous):
+                picks_object["pos"] += [index]
+                picks_object["peaks"] += [value]
+                is_plateous = False
+                continue
+            picks_object["pos"] += [i]
+            picks_object["peaks"] += [arr[i]]
+            continue
+
+        if(arr[i] == arr[i+1]):
+            if(not is_plateous):
+                is_plateous = True
+                index = i
+                value = arr[i]
+            continue
+        
+        is_plateous = False
+
+    return picks_object
+
+
+print(pick_peaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]))
+
+
+# CODIGO CON MEJORES PRACTICAS
+def pick_peaks(arr):
+    pos = []
+    prob_peak = False
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i-1]:
+            prob_peak = i
+        elif arr[i] < arr[i-1] and prob_peak:
+            pos.append(prob_peak)
+            prob_peak = False
+    return {'pos':pos, 'peaks':[arr[i] for i in pos]}
+
+def pick_peaks(arr):
+    peak, pos = [], []
+    res = { "peaks":[], "pos":[] }
+    
+    for i in range(1, len(arr)) :
+        if arr[i]>arr[i-1] :
+            peak, pos = [arr[i]], [i]
+        
+        elif arr[i]<arr[i-1] :
+            res["peaks"] += peak
+            res["pos"] += pos
+            peak, pos = [], []
+    
+    return res
